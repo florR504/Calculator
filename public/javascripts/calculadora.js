df5b2c7377321f30e1b25dddfd7b7961ff0a78af
Flor <florenciar504@gmail.com>
2022-11-09 18:13:44 -0300

    calculadora funcional

--- public/javascripts/calculadora.js
+++ public/javascripts/calculadora.js
@@ -1,24 +1,24 @@
 window.addEventListener('load', function(){
-let $input = document.getElementById('input');
-let $mas = document.getElementById('mas');
-let $menos = document.getElementById('menos');
-let $por = document.getElementById('por');
-let $division = document.getElementById('division');
-let $igual = document.getElementById('igual');
-let $uno = document.getElementById('uno');
-let $numbers = document.querySelectorAll('.numbers')
-let number = [];
-function value(event){
-    console.log(event.target.value);
+ let $buttons = document.querySelectorAll('.button__calculadora');
+ let $answer = document.querySelector('.input__calculadora');
+ let $result = document.querySelector('#igual');
+ let $clear = document.querySelector('#clear')
+ console.log($result);
+ 
+ $buttons.forEach(button =>{
+    button.addEventListener('click', () =>{
+        $answer.value += button.value
+    })
+ })
+
+function evalResult(){
+    $answer.value = eval($answer.value)
 }
-for(let i = 0; i < $numbers.length; i++){
-   number.push($numbers[i].value)
-    console.log(number);
+function clearOutput(){
+    $answer.value = ''
 }
-console.log(number);
-number.addEventListener('click', value)
 
-
-
+ $result.addEventListener('click', evalResult)
+ $clear.addEventListener('click', clearOutput)
 
 })
