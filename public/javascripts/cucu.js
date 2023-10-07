ba3e258438787b75f10960c5764647fb761e571d
Flor Rodriguez <florenciar504@gmail.com>
2023-10-07 19:09:18 -0300

    Componente Cucu

---
+++ public/javascripts/cucu.js
@@ -0,0 +1,6 @@
+let arrayDeCucus = ['cucu', 'cuquito', 'pollito', 'lorito chiquito']
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log('Te amo ' + element)
+    
+}
